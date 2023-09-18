a548865992afe27ba52d247012c40942cf00c5e3
kaushal kumar <kaushalkumar50009@gmail.com>
2023-09-18 21:07:12 +0530

    fetch data and showing random joke

---
+++ index.js
@@ -0,0 +1,39 @@
+const content = document.getElementById("text_content");
+const btn = document.getElementById("btn");
+
+const apiUrl =
+  "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let getJoke = async () => {
+  await fetch(apiUrl)
+    .then((res) => res.json())
+
+
+    .then((data) => {
+      console.log(data);
+
+      content.innerHTML = `${data.joke}`;
+    });
+
+};
+
+btn.addEventListener("click", function () {
+  getJoke();
+});
+
+
+getJoke();
+
+// (async function () {
+//   await fetch(apiUrl)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
+// })();
+
+function kau(bab) {
+  return a
+}
+
+kau(23)
